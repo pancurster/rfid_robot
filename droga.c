7463ad4cfa42b8941516c8f5c5dd42e2f38782b5
Lukasz Panek <pancurster@gmail.com>
2011-07-10 20:38:26 +0200

    Drobne poprawki, zmienne globalne

--- droga.c
+++ droga.c
@@ -4,7 +4,16 @@ void dijkstra(int);
 void drukuj_wyniki(int*, int*);
 int min(int*, int, int*);
 
-int Q[][16] =
+#define LICZBA_WEZLOW 16
+
+//maksymalna liczba sasiadow dla siatki prostokatnej
+#define MAX_SASIADOW_PROSTOKAT 4
+//maksymalna l. sasiadow dla siatki trojkatnej
+#define MAX_SASIADOW_TROJKAT 8
+
+#define NIESKACZONOSC 555
+
+int Q[][LICZBA_WEZLOW] =
 {
     {1,4,666,666}, {0,2,5,666}, {1,3,6,666}, {2,7,666,666},
     {0,5,8,666}, {1,6,9,4}, {2,7,10,5}, {3,11,6,666},
@@ -12,8 +21,7 @@ int Q[][16] =
     {8,13,666,666}, {9,14,12,666}, {10,15,13,666}, {11,14,666,666}
 };
 
-const int NIESKACZONOSC = 555;
-
+/* --------------------------------- */
 int main(int argc, char* argv[])
 {
     if(argc > 1)
@@ -22,6 +30,7 @@ int main(int argc, char* argv[])
         dijkstra(0);
     return 0;
 }
+
 int min(int* tab, int size, int* inS)
 {
     int m = 666; //pseudo nieskonczonosc :)
@@ -38,12 +47,12 @@ int min(int* tab, int size, int* inS)
 }
 
 void dijkstra(int cel){
-    int d[16];
-    int p[16] = {0};
-    int inS[16] = {0}; //tablica informujaca o obecnosci w zbiorze Q
+    int d[LICZBA_WEZLOW];
+    int p[LICZBA_WEZLOW] = {0};
+    int inS[LICZBA_WEZLOW] = {0}; //tablica informujaca o obecnosci w zbiorze Q
 
     int i,j = 0;
-    for(i=0; i < 16; i++){
+    for(i=0; i < LICZBA_WEZLOW; i++){
         d[i] = NIESKACZONOSC; // 555 oznacza nieskonczonosc
     }
 
@@ -52,15 +61,15 @@ void dijkstra(int cel){
 
     int h = 0; //pomocnicza
     int v; //aktualnie badany wezel
-    for(i = 0; i < 16; i++){
-        v = min(d, 16, inS); //index najmniejszego wezla
+    for(i = 0; i < LICZBA_WEZLOW; i++){
+        v = min(d, LICZBA_WEZLOW, inS); //index najmniejszego wezla
         /* przsuwamy wezel do zbioru S */
         inS[v] = 1;
 
         /* sasiedzi */
         h = d[v] + 1;
         if(h > d[v]){ //wazny warunek
-            for(j=0; j < 4; j++){
+            for(j=0; j < MAX_SASIADOW_PROSTOKAT; j++){
                 if( (Q[v][j] != 666) && (inS[ Q[v][j] ] != 1) ){ // warunki: istnieje sasiad i nie nalezy do S
                     p[Q[v][j]] = v; //poprzenik
                     d[Q[v][j]] = h; //waga + waga poprzednika
@@ -74,18 +83,18 @@ void dijkstra(int cel){
 
 void drukuj_wyniki(int* d, int* p){
     int x = 0;
-    for(x=0; x < 16; x++){
+    for(x=0; x < LICZBA_WEZLOW; x++){
         printf("%i ", x);
     }
     printf("\n");
 
-    for(x=0; x < 16; x++){
+    for(x=0; x < LICZBA_WEZLOW; x++){
         printf("%i ", d[x]);
     }
     
     printf("\n");
 
-    for(x=0; x < 16; x++){
+    for(x=0; x < LICZBA_WEZLOW; x++){
         printf("%i ", p[x]);
     }
 }
