2d3221a8c506fd31d1475920b90b5d548eaf08f9
Lukasz Panek <pancurster@gmail.com>
2011-07-14 19:51:43 +0200

    funkcje wyszukiwania nr wezla na podstawie id kart RFID, kilka innych zmian

--- droga.c
+++ droga.c
@@ -17,6 +17,10 @@
 #define NO_DEF 777
 #define INF 666
 
+/* Globalna tablica poprzednikow wyliczana w algorytmie dijkstry.
+ * Wedlug niej laczymy wezly tworzac scierzke do wezla 'cel'*/
+int P[LICZBA_WEZLOW] = {0};
+
 int Q_P[][LICZBA_WEZLOW] =
 {
     {1,4,INF,INF}, {0,2,5,INF}, {1,3,6,INF}, {2,7,INF,INF},
@@ -33,6 +37,12 @@ int Q_T[][LICZBA_WEZLOW] =
     {8,9,INF,INF}, {9,10,INF,INF}, {10,11,INF,INF}, {11,INF,INF,INF}
 };
 
+struct wektor_ruchu{
+    int pp;
+    int ap;
+    int np;
+}POZ;
+
 /* Kierunki */
 #define N 1
 #define E 2
@@ -40,11 +50,16 @@ int Q_T[][LICZBA_WEZLOW] =
 #define W 7
 
 /*********** HARDWARE *************/
-
+    
 const uint8_t SILNIK_LEWY = 2;
 const uint8_t SILNIK_PRAWY = 3;
 const uint8_t SILNIKI_AKTYWNE = 4;
 
+/* Zwiazane z funkcja odczytaj_karte */
+#define CZYTAJ_DO_SKUTKU 0
+#define PROBKUJ 1
+
+/* SILNIKI */
 #define START HIGH
 #define STOP LOW
 #define SILNIKI_LEWY_PRAWY 23
@@ -52,9 +67,15 @@ const uint8_t SILNIKI_AKTYWNE = 4;
 
 volatile int DATA = 0;
 int STARTSTOP = 0;
-char METODA_STEROWANIA = 'N';
-int CEL = 0;
 
+/* Przykladowe nr kart, posortowane! */
+int ID_KART[LICZBA_WEZLOW] = 
+{ 
+    15,16,23,25,
+    28,34,36,41,
+    42,47,49,58,
+    68,73,78,93
+};
 /***** KONIEC DEKLARACJI **********/
 
 
@@ -63,6 +84,8 @@ void drukuj_wyniki(int*, int*);
 int minimum(int*, int, int*);
 int kierunek(int, int);
 void decyzja(int, int, int);
+int znajdz_nr_wezla(int, int, int);
+int numer_wezla(int);
 
 void stop();
 void skrecajLewo();
@@ -74,13 +97,41 @@ void loop();
 #ifdef PC_DEBUG
 int main(int argc, char* argv[])
 {
+    /*
     if(argc > 1)
         dijkstra(atoi(argv[1]), Q_T);
     else
         dijkstra(0, Q_P);
+        */
+    printf("Index tej kartu to: %i\n", znajdz_wezel(atoi(argv[1]),0,15));
     return 0;
 }
 #endif
+
+/* uproszczenie wywolania fun. znajdz_nr_wezla.
+ * Wyszukuje w fizycznych numerach kart RFID index danej karty */
+int numer_wezla(int id)
+{
+    return znajdz_nr_wezla(id, 0, LICZBA_WEZLOW-1);
+}
+
+/* Przeszukiwanie binarne */
+int znajdz_nr_wezla(int id, int poczatek, int koniec)
+{
+    if( poczatek > koniec )
+        return -1;
+
+    int index = (poczatek + koniec) / 2;
+    if( id == ID_KART[index] ){
+        return index;
+    }
+    else if( id > ID_KART[index] ){
+        return znajdz_nr_wezla(id, index +1, koniec);
+    }
+    else if( id < ID_KART[index] ){
+        return znajdz_nr_wezla(id, poczatek, index -1);
+    }
+}
 
 /* Okresla kierunek na podstawie punktu poczatkowego
  * i punktu nastepnego */
@@ -166,7 +217,6 @@ int minimum(int* tab, int size, int* inS)
 
 void dijkstra(int cel, int Q[][LICZBA_WEZLOW]){
     int d[LICZBA_WEZLOW];
-    int p[LICZBA_WEZLOW] = {0};
     int inS[LICZBA_WEZLOW] = {0}; //tablica informujaca o obecnosci w zbiorze Q
 
     int i,j = 0;
@@ -176,7 +226,7 @@ void dijkstra(int cel, int Q[][LICZBA_WEZLOW]){
     }
 
     d[cel] = 0; //droga do celu z niego samego jest rowna 0
-    p[cel] = cel; //cel nie ma poprzednika
+    P[cel] = cel; //cel nie ma poprzednika
 
     int h = 0; //pomocnicza
     int v; //aktualnie badany wezel
@@ -190,7 +240,7 @@ void dijkstra(int cel, int Q[][LICZBA_WEZLOW]){
         if(h > d[v]){ //wazny warunek
             for(j=0; j < MAX_SASIADOW_PROSTOKAT; j++){
                 if( (Q[v][j] != INF) && (inS[ Q[v][j] ] != 1) ){ // warunki: istnieje sasiad i nie nalezy do S
-                    p[Q[v][j]] = v; //poprzenik
+                    P[Q[v][j]] = v; //poprzenik
                     d[Q[v][j]] = h; //waga + waga poprzednika
                 }
             }
@@ -198,7 +248,7 @@ void dijkstra(int cel, int Q[][LICZBA_WEZLOW]){
 
     }
 #ifdef PC_DEBUG
-    drukuj_wyniki(d,p);
+    drukuj_wyniki(d,P);
 #endif
 }
 
@@ -228,54 +278,6 @@ void drukuj_wyniki(int* d, int* p){
  */
 #ifndef PC_DEBUG
 
-void setup(){
-    /* inicjalizacja komunikacji szerogowej z modulem RFID */
-    Serial.begin(9600);
-
-    /* okreslenie pinow sterujacych silnikami */
-    pinMode(SILNIK_LEWY, OUTPUT);
-    pinMode(SILNIK_PRAWY, OUTPUT);
-    pinMode(SILNIKI_AKTYWNE, OUTPUT);
-
-    /* Piny dipSwitcha sa wejsciami */
-    pinMode(A2, INPUT);
-    pinMode(A3, INPUT);
-    pinMode(A4, INPUT);
-    pinMode(A5, INPUT);
-
-    /* Odczyt rodzaju siatki/sterowania, lub celu */
-    if( digitalRead(A2) ){
-        METODA_STEROWANIA = 'P';
-    }
-    else if( digitalRead(A3) ){
-        METODA_STEROWANIA = 'T';
-    }
-    else if( digitalRead(A4) ){
-        METODA_STEROWANIA = 'N';
-    }
-    else if( digitalRead(A5) ){
-        // ODCZYTAJ KARTE CELU
-    }
-    else{
-        METODA_STEROWANIA = 'N';
-    }
-
-    /* Generowanie tablicy kolejnych wezlow prowadzacych do celu,
-     * lub w przypadku sterowania nadarznego: brak akcji */
-    if( METODA_STEROWANIA == 'P'){
-        dijkstra(CEL, Q_P);
-    }
-    else if( METODA_STEROWANIA == 'T'){
-        dijkstra(CEL, Q_T);
-    }
-    else if( METODA_STEROWANIA == 'N'){
-        break;
-    }
-
-    /* Po starcie pojazd nie porusza sie */
-    digitalWrite(SILNIKI_AKTYWNE, LOW);
-    
-}
 /*
  * motor: SILNIK_LEWY, SILNIK_PRAWY, SILNIK_LEWY_PRAWY
  * command: START, STOP
@@ -325,6 +327,85 @@ void stop(){
     }
 }
 
+/* TODO UWAGA: ta funkcje trzeba przetestwac pod katem 
+ * narzutu czasowego */
+int odczytaj_karte(char probkuj)
+{
+    volatile int data = 0;
+    char done = 0;
+    if(probkuj)
+        done = 1;
+
+    do{
+        if(Serial.available() > 0){
+            data = Serial.read();
+            Serial.flush();
+            done = 1;
+            return data;
+        }
+    }while(!done);
+}
+
+void setup(){
+    /* inicjalizacja komunikacji szerogowej z modulem RFID */
+    Serial.begin(9600);
+
+    /* okreslenie pinow sterujacych silnikami */
+    pinMode(SILNIK_LEWY, OUTPUT);
+    pinMode(SILNIK_PRAWY, OUTPUT);
+    pinMode(SILNIKI_AKTYWNE, OUTPUT);
+
+    /* Piny dipSwitcha sa wejsciami */
+    pinMode(A2, INPUT);
+    pinMode(A3, INPUT);
+    pinMode(A4, INPUT);
+    pinMode(A5, INPUT);
+
+    /* Numer wezla 'celu'.
+     * Jesli nie jest ustawiany to cel = wezel nr 0 */
+    int CEL = 0;
+
+    char METODA_STEROWANIA = 'N';
+
+    /* Odczyt rodzaju siatki/sterowania, lub celu */
+    if( digitalRead(A2) ){
+        METODA_STEROWANIA = 'P';
+    }
+    else if( digitalRead(A3) ){
+        METODA_STEROWANIA = 'T';
+    }
+    else if( digitalRead(A4) ){
+        METODA_STEROWANIA = 'N';
+    }
+    else if( digitalRead(A5) ){
+        CEL = numer_wezla(odczytaj_karte(CZYTAJ_DO_SKUTKU));
+    }
+    else{
+        /* w przypadku gdyby zaden przelacznik nie byl przelaczony */
+        METODA_STEROWANIA = 'N';
+    }
+
+    /* Generowanie tablicy kolejnych wezlow prowadzacych do celu,
+     * lub w przypadku sterowania nadarznego: brak akcji */
+    if( METODA_STEROWANIA == 'P'){
+        dijkstra(CEL, Q_P);
+    }
+    else if( METODA_STEROWANIA == 'T'){
+        dijkstra(CEL, Q_T);
+    }
+
+    /* Po starcie pojazd nie porusza sie */
+    digitalWrite(SILNIKI_AKTYWNE, LOW);
+
+    /* Inicjalizajcia struktury z informacja o
+     * pozycjach robota */
+    POZ.pp = -1;
+    if( POZ.ap = numer_wezla(odczytaj_karte(0)) == -1)
+        //TODO tutaj powinna byc obsluga bledu
+    POZ.np = P[POZ.ap];
+
+}
+
 /*
  * GLOWNA PETLA PROGRAMU
  */
@@ -336,7 +417,7 @@ void loop(){
        if(DATA == 55) { skrecajLewo(); } 
        if(DATA == 49) { stop(); }
        Serial.flush();
-   } 
+   }
 
 }
 
